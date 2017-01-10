93507a5a2e48006545a59e6d9a7f58f79f031806
Matt Ryder <m.ryder@volcanic.co.uk>
2017-01-10 12:53:47 +0000

    Fix bug using Rails method in Ruby for rails/configure.rb

--- rails/recipes/configure.rb
+++ rails/recipes/configure.rb
@@ -41,7 +41,7 @@ node[:deploy].each do |application, deploy|
     notifies :run, "execute[restart Rails app #{application}]"
 
     only_if do
-      deploy[:memcached][:host].present? && File.directory?("#{deploy[:deploy_to]}/shared/config/")
+      deploy.has_key?(:memcached) && deploy[:memcached].has_key?(:host) && File.directory?("#{deploy[:deploy_to]}/shared/config/")
     end
   end
 end
